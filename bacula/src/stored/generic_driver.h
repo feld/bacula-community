8358f584665de4224f8595ee0e3888eed8a22774
Norbert Bizet <norbert.bizet@baculasystems.com>
2020-05-12 19:00:43 +0200

    BEE Backport bacula/src/stored/generic_driver.h
    This commit is the result of the squash of the following main commits:
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Fri Sep 20 08:56:28 2019 -0400
    
        cloud: Do not overwrite an existing part in the cloud
    
        cloud: s3 copy when part exists in cloud. cleanup on truncate.
    
        cloud: move part versionning to cloud_dev.
    
        make sure move and cleanup is done from cloud_dev.
        A callback is passed to clean_cloud_volume to test if a cloud file
        needs to be cleaned.
    
        cloud: implement S3 for part versionning and cleanup
    
        cloud : file driver versionning implementation.
    
        cloud: fix Job message.
    
        cloud: move POOLMEM allocation
    
        cloud: update move interface.
        Implement was_cloud_driver.
    
        Cloud: implement exists in move_cloud_part for s3
    
        cloud: minor bug fixes.
    
        cloud: adapt google cloud driver to new paradigm
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Thu Jun 27 10:19:59 2019 -0400
    
        cloud: Start drivers and log into the working directory
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Mon Jun 24 07:52:56 2019 -0400
    
        cloud: mv debug level to 50 and add a DT_CLOUD tag
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Thu Jun 6 07:40:19 2019 -0400
    
        cloud: Forward bandwidth limit to generic driver
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Tue May 14 05:46:36 2019 -0400
    
        cloud: Fix #3914 unset LC_TIME when using was driver
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Fri Mar 22 10:53:57 2019 +0100
    
        cloud: allow use of file path for was drv
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Tue Mar 12 09:25:35 2019 +0100
    
        cloud: generic drive sets azure params
    
    Author: Eric Bollengier <eric@baculasystems.com>
    Date:   Mon Jan 28 10:40:15 2019 +0100
    
        Update copyright year
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Fri Sep 28 13:10:07 2018 +0200
    
        cloud: forward Bacula cloud directives to cloud drivers
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Sat Aug 4 11:50:12 2018 +0200
    
        cloud: Forward host name. Used as cache path in generic_cloud_driver
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Fri Jul 13 10:11:37 2018 +0200
    
        cloud: Take script return value in account. Handle new bpipe "e" mode
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Fri Jul 6 11:59:02 2018 +0200
    
        cloud: allow throttling and  fix some code issues with generic driver
    
        Take some Eric's remarks in account.
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Wed Jul 4 11:03:41 2018 +0200
    
        cloud: introduce Google cloud implementation
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Mon Jul 2 11:58:47 2018 +0200
    
        cloud: implement async I/O with generic driver scripts
    
        stdin and stdout are used for piped IPC with the script. call_fct is the function that handles file descriptors.
        Specific callback are passed for each function.
    
    Author: Norbert Bizet <norbert.bizet@baculasystems.com>
    Date:   Thu Jun 21 18:19:05 2018 +0200
    
        cloud: First implementation and scenario test for generic_driver

---
+++ bacula/src/stored/generic_driver.h
@@ -0,0 +1,151 @@
+/*
+   Bacula(R) - The Network Backup Solution
+
+   Copyright (C) 2000-2020 Kern Sibbald
+
+   The original author of Bacula is Kern Sibbald, with contributions
+   from many others, a complete list can be found in the file AUTHORS.
+
+   You may use this file and others of this release according to the
+   license defined in the LICENSE file, which includes the Affero General
+   Public License, v3.0 ("AGPLv3") and some additional permissions and
+   terms pursuant to its AGPLv3 Section 7.
+
+   This notice must be preserved when any source code is
+   conveyed and/or propagated.
+
+   Bacula(R) is a registered trademark of Kern Sibbald.
+*/
+/*
+ * Routines for writing Cloud drivers
+ *
+  * Written by Norbert Bizet, June MMXVIII
+ *
+ */
+#ifndef GENERIC_CLOUD_DRIVER_H
+#define GENERIC_CLOUD_DRIVER_H
+
+#include "cloud_driver.h"
+
+class generic_driver: public cloud_driver {
+
+protected:
+
+   uint32_t buf_len;
+   char *host_name;
+   char *bucket_name;
+   char *access_key;
+   char *secret_key;
+   char *region;
+   int32_t protocol;
+   int32_t uri_style;
+   char *driver_command;
+   char *blob_endpoint;
+   char *file_endpoint;
+   char *queue_endpoint;
+   char *table_endpoint;
+   char *endpoint_suffix;
+   uint32_t max_concurrent_uploads;
+   uint32_t max_concurrent_downloads;
+   /* 
+      read_cb_type : returns the number of proceed bytes from char*. 
+      char* is the data buffer. 
+      size_t is the buffer size. 
+      void* is arg (opaque handler)
+   */
+   typedef size_t (read_cb_type)(char*, size_t, void*);
+   typedef struct {
+      read_cb_type* fct;
+      void *arg;
+   } read_callback;
+
+   /* 
+      write_cb_type : returns the number of proceed bytes from char*. 
+      char* is the data buffer. 
+      size_t is the buffer size. 
+      void* is arg (opaque handler)
+      int& is the cb file status.
+   */
+   #define WRITE_CB_EOF 1
+   #define WRITE_CB_ERR 2
+   typedef size_t (write_cb_type)(char*, size_t, void*, int&);
+   typedef struct {
+      write_cb_type* fct;
+      void *arg;
+   } write_callback;
+
+   /* accessors with difference prototypes */
+   int call_fct( const char* fct_name,
+                  const char* volume_name,
+                  int part_number,
+                  read_callback *read_cb,
+                  write_callback *write_cb,
+                  cancel_callback *cancel_cb,
+                  POOLMEM *&err,
+                  const char* cache_path_name = NULL
+                  );
+   int call_fct(const char* fct_name,
+                  const char* volume_name,
+                  const char* part_name,
+                  read_callback *read_cb,
+                  write_callback *write_cb,
+                  cancel_callback *cancel_cb,
+                  POOLMEM *&err);
+
+   /* actual working function */
+   int call_fct( char* cmd,
+                  read_callback *read_cb,
+                  write_callback *write_cb,
+                  cancel_callback *cancel_cb,
+                  POOLMEM *&err);
+
+public:
+   generic_driver();
+   virtual ~generic_driver();
+   bool copy_cache_part_to_cloud(transfer *xfer);
+   bool move_cloud_part(const char *VolumeName, uint32_t apart , const char *to, cancel_callback *cancel_cb, POOLMEM *&err, int& exists);
+   int copy_cloud_part_to_cache(transfer *xfer);
+   bool restore_cloud_object(transfer *xfer, const char *cloud_fname);
+   bool is_waiting_on_server(transfer *xfer);
+   bool truncate_cloud_volume(const char *volume_name, ilist *trunc_parts, cancel_callback *cancel_cb, POOLMEM *&err);
+   bool clean_cloud_volume(const char *VolumeName, cleanup_cb_type *cb, cleanup_ctx_type *ctx, cancel_callback *cancel_cb, POOLMEM *&err);
+   bool init(CLOUD *cloud, POOLMEM *&err);
+   bool term(POOLMEM *&err);
+   bool start_of_job(POOLMEM *&err);
+   bool end_of_job(POOLMEM *&err);
+   bool get_cloud_volume_parts_list(const char* volume_name, ilist *parts, cancel_callback *cancel_cb, POOLMEM *&err);
+   bool get_cloud_volumes_list(alist *volumes, cancel_callback *cancel_cb, POOLMEM *&err);
+
+private:
+   char *edit_device_codes(
+      char *omsg,
+      const char *cmd, 
+      const char *cache_path_name = 0,
+      const char *cloud_path_name = 0,
+      const char *volume_name = 0);
+
+private:
+   POOLMEM *access_key_env;
+   POOLMEM *secret_key_env;
+   POOLMEM *bucket_env;
+   POOLMEM *host_env;
+   POOLMEM *region_env;
+   POOLMEM *protocol_env;
+   POOLMEM *uri_style_env;
+   POOLMEM *blob_endpoint_env;
+   POOLMEM *file_endpoint_env;
+   POOLMEM *queue_endpoint_env;
+   POOLMEM *table_endpoint_env;
+   POOLMEM *endpoint_suffix_env;
+   POOLMEM *max_concurrent_uploads_env;
+   POOLMEM *max_concurrent_downloads_env;
+   POOLMEM *upload_limit_env;
+   POOLMEM *download_limit_env;
+   POOLMEM *unset_lctime_env;
+   POOLMEM *debug_env;
+   POOLMEM *working_path_env;
+   POOLMEM *home_path_env;
+   char *envs[21];
+};
+
+#endif /* GENERIC_CLOUD_DRIVER_H*/
