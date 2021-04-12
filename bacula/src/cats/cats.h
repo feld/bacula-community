1020b2afa133668c0c62dee21f7caf497db85198
Michal Rakowski <michal.rakowski@baculasystems.com>
2021-04-12 17:11:33 +0200

    catalog: update to ver 1024

--- bacula/src/cats/cats.h
+++ bacula/src/cats/cats.h
@@ -48,7 +48,7 @@
  */
 
 /* Current database version number for all drivers */
-#define BDB_VERSION 1023
+#define BDB_VERSION 1024
 
 typedef void (DB_LIST_HANDLER)(void *, const char *);
 typedef int (DB_RESULT_HANDLER)(void *, int, char **);
