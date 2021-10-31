b4eab4415e68c6120be9ebcb21ea89d8d29ea69a
Marcin Haba <marcin.haba@bacula.pl>
2021-10-31 14:00:55 +0100

    baculum: Update version

--- gui/baculum/protected/Common/Class/Params.php
+++ gui/baculum/protected/Common/Class/Params.php
@@ -31,7 +31,7 @@ Prado::using('Application.Common.Class.CommonModule');
  */
 class Params extends CommonModule {
 
-	const BACULUM_VERSION = '11.0.5.4';
+	const BACULUM_VERSION = '11.0.5.5';
 
 	public static $months = array(
 		'jan' => 'January',
