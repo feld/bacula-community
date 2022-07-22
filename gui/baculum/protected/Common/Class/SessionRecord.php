181b2e2bbe382f1c55e9707a4d4e11c15ab157ca
Marcin Haba <marcin.haba@bacula.pl>
2022-07-22 03:20:54 +0200

    baculum: Fix #2653 PHP warning about wrong array_key_exists() parameter in session record

--- gui/baculum/protected/Common/Class/SessionRecord.php
+++ gui/baculum/protected/Common/Class/SessionRecord.php
@@ -89,7 +89,8 @@ class SessionRecord extends CommonModule implements SessionItem {
 				$fp = fopen($sessfile, 'r');
 				if (flock($fp, LOCK_SH, $wouldblock)) {
 					$content = file_get_contents($sessfile);
-					$GLOBALS['sess'] = unserialize($content);
+					$ucont = unserialize($content);
+					$GLOBALS['sess'] = is_array($ucont) ? $ucont : array();
 					flock($fp, LOCK_UN);
 				} else {
 					$emsg = 'Unable to shared lock ' . $sessfile;
