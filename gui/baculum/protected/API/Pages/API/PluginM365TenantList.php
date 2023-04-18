2c7c141d983745c33e83138cacb1eebcfbd6fcf4
Marcin Haba <marcin.haba@bacula.pl>
2023-04-18 09:57:54 +0200

    baculum: Add tenant indentifier list endpoint

---
+++ gui/baculum/protected/API/Pages/API/PluginM365TenantList.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Bacula(R) - The Network Backup Solution
+ * Baculum   - Bacula web interface
+ *
+ * Copyright (C) 2013-2023 Kern Sibbald
+ *
+ * The main author of Baculum is Marcin Haba.
+ * The original author of Bacula is Kern Sibbald, with contributions
+ * from many others, a complete list can be found in the file AUTHORS.
+ *
+ * You may use this file and others of this release according to the
+ * license defined in the LICENSE file, which includes the Affero General
+ * Public License, v3.0 ("AGPLv3") and some additional permissions and
+ * terms pursuant to its AGPLv3 Section 7.
+ *
+ * This notice must be preserved when any source code is
+ * conveyed and/or propagated.
+ *
+ * Bacula(R) is a registered trademark of Kern Sibbald.
+ */
+
+use Baculum\Common\Modules\Errors\ClientError;
+use Baculum\Common\Modules\Errors\PluginError;
+use Baculum\Common\Modules\Errors\PluginM365Error;
+
+/**
+ * List Microsoft 365 plugin tenant identifiers.
+ *
+ * @author Marcin Haba <marcin.haba@bacula.pl>
+ * @category API
+ * @package Baculum API
+ */
+class PluginM365TenantList extends BaculumAPIServer {
+
+	public function get() {
+		$misc = $this->getModule('misc');
+		$client = null;
+		$clientid = $this->Request->contains('id') ? (int)$this->Request['id'] : 0;
+		$result = $this->getModule('bconsole')->bconsoleCommand(
+			$this->director,
+			['.client'],
+			null,
+			true
+		);
+		if ($result->exitcode === 0) {
+			$client_val = $this->getModule('client')->getClientById($clientid);
+			if (is_object($client_val) && in_array($client_val->name, $result->output)) {
+				$client = $client_val->name;
+			} else {
+				$this->output = ClientError::MSG_ERROR_CLIENT_DOES_NOT_EXISTS;
+				$this->error = ClientError::ERROR_CLIENT_DOES_NOT_EXISTS;
+				return;
+			}
+		} else {
+			$this->output = PluginError::MSG_ERROR_WRONG_EXITCODE;
+			$this->error = PluginError::ERROR_WRONG_EXITCODE;
+			return;
+		}
+
+		$tenants = $this->getModule('fd_plugin_cfg')->getConfig('m365', $client);
+
+		$this->output = array_keys($tenants);
+		$this->error = PluginM365Error::ERROR_NO_ERRORS;
+	}
+}
